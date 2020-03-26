df01058fdee08831ac380065d105d385fcfe22ed
Amel <zekafrombtm@hotmail.com>
2020-03-26 10:15:15 +0100

    1234

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from '../../components/sing-in/sing-in.component';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div>
+        <SignIn />
+    </div>
+);
+
+
+
+export default SignInAndSignUpPage;
