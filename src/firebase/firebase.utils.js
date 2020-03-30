a764a390d4ce18806abacf46cab1f4751f3564a1
Amel <zekafrombtm@hotmail.com>
2020-03-30 15:32:35 +0200

    implemented sign up with user email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,40 @@ const config = {
     appId: "1:631141621239:web:75e7034b78fa0a97f2e2f2"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get()
+
+    if(!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+          await userRef.set({
+            displayName,
+            email,
+            createdAt,
+            ...additionalData
+          })
+
+      } catch (error){
+        console.log('error creating user',error.message);
+
+
+      }
+    }
+
+    return userRef;
+
+    console.log(snapShot);
+
+    console.log(firestore.doc('users/125125f'));
+
+  }
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import SignIn from '../../components/sing-in/sing-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
-    <div>
+    <div className="sign-in-and-sign-up">
         <SignIn />
+        <SignUp />
+
     </div>
 );
 
