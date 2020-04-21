d42ac38b2ecf67558ab784fe0192daaf89822de0
Amel <zekafrombtm@hotmail.com>
2020-04-21 15:29:44 +0200

    shop page with own reducers and selectors

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -21,9 +21,9 @@ const { name, imageUrl, quantity, price } = cartItem;
 
         <span className="name">{name}</span>
         <span className="quantity">
-            <div class="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+            <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
             <span className="value">{quantity}</span>
-            <div class="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
+            <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
             </span>
         <span className="price">{price}</span>
         <div className="remove-button" onClick={() => clearItem(cartItem)}>
