861a7c5a67e5a1d7e903be681202c76293eeaaa3
Amel <zekafrombtm@hotmail.com>
2020-05-01 15:28:43 +0200

    Updates

---
+++ precache-manifest.6d5d429ccff869fdd6fb3c0bc7ae8f2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122e51671192670e5838ff3206c3378a",
+    "url": "/clothingapp/index.html"
+  },
+  {
+    "revision": "5f2c4f917f72a6ff5a7f",
+    "url": "/clothingapp/static/css/main.4f1cc6da.chunk.css"
+  },
+  {
+    "revision": "ae734e0af5a8350aa8fa",
+    "url": "/clothingapp/static/js/2.8cc1c49d.chunk.js"
+  },
+  {
+    "revision": "ad61e5a3703d8af976e070e9896b739a",
+    "url": "/clothingapp/static/js/2.8cc1c49d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2c4f917f72a6ff5a7f",
+    "url": "/clothingapp/static/js/main.f7236162.chunk.js"
+  },
+  {
+    "revision": "9141d1fd46db44a6bad4",
+    "url": "/clothingapp/static/js/runtime-main.bee5e759.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothingapp/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/clothingapp/static/media/shopping-bag.30f07071.svg"
+  }
+]);
